70d1bf7abd993f51def0f99a48d3ad1fec8c098d
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-05-28 20:21:38 -0300

    Finalizado

--- fifo.v
+++ fifo.v
@@ -15,7 +15,41 @@ module fifo(
     // status
     output reg [3:0] fifo_words  // Current number of elements
 );
+    localparam DATA_WIDTH  = 8;
+    localparam FIFO_DEPTH  = 8;
 
-//insira seu código aqui
+    reg [DATA_WIDTH-1:0] mem [0:FIFO_DEPTH-1];
+    reg [DATA_WIDTH-1:0] write_ptr;
+    reg [DATA_WIDTH-1:0] read_ptr;
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            write_ptr <= 8'd0;
+        end else if (wr_en && !full) begin
+            mem[write_ptr] <= data_in;
+            write_ptr <= write_ptr + 1;
+        end
+    end
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            read_ptr <= 8'd0;
+        end else if (rd_en && !empty) begin
+            data_out <= mem[read_ptr];
+            read_ptr <= read_ptr + 1;
+        end
+    end
+
+    always @(*) begin
+        if (write_ptr >= read_ptr)
+            fifo_words = write_ptr - read_ptr;
+        else
+            fifo_words = write_ptr - read_ptr;
+    end
+
+    assign full  = ((write_ptr == read_ptr - 1) || 
+                   (write_ptr == FIFO_DEPTH && read_ptr == 0));
+    assign empty = (write_ptr == read_ptr);
 
 endmodule
+
--- fsm.v
+++ fsm.v
@@ -8,7 +8,52 @@ module fsm(
     
     input [3:0] fifo_words
 );
+    localparam STOP = 2'b00;
+    localparam WRITE = 2'b01;
+    localparam READ = 2'b10;
 
-// insira seu código aqui
+    reg [1:0] estado, proximo_estado;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n && estado !== STOP) begin
+            estado <= STOP;
+        end else begin
+            estado <= proximo_estado;
+        end
+    end
+
+    localparam WORDS = 5;
+
+    always @(*) begin
+        proximo_estado <= estado; 
+
+        if (estado == STOP) begin
+            proximo_estado <= WRITE;
+        end
+        else if (estado == WRITE) begin
+            if (fifo_words >= WORDS)
+                proximo_estado <= READ;
+            else
+                proximo_estado <= WRITE;
+        end
+        else if (estado == READ) begin
+            if (fifo_words <= 4'd2)
+                proximo_estado <= WRITE;
+            else
+                proximo_estado <= READ;
+        end
+    end
+
+    reg [7:0] FIFO_DATA;
+    assign fifo_data = FIFO_DATA;
+
+    always @(*) begin
+        FIFO_DATA <= 8'hAA;
+        case (estado)
+            WRITE: wr_en = 1;
+            READ: wr_en = 0;
+        endcase
+    end
 
 endmodule
+
